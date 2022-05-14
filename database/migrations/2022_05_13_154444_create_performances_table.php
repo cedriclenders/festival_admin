cb5ff2967063ee903b5d5e43d817c67471d438be
Cedric Lenders <ceelenders@hotmail.com>
2022-05-14 13:49:43 +0200

    migrations + relationships

---
+++ database/migrations/2022_05_13_154444_create_performances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('performer_id')->constrained();
+            $table->foreignId('stage_id')->constrained();
+            $table->foreignId('timeslot_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+         Role::create(['name' => 'Admin']);
+         Role::create(['name' => 'User']);
     }
 }
