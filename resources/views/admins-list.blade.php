e07c3b70c5d58c124cfe03953a29ac0cf11d64f1
Cedric Lenders <ceelenders@hotmail.com>
2022-06-17 18:03:14 +0200

    cleaning up admins-list

--- resources/views/admins-list.blade.php
+++ resources/views/admins-list.blade.php
@@ -20,10 +20,15 @@
                     <td>
                         <!-- Call to action buttons -->
                         <ul class="list-inline m-0">
-                            <li class="list-inline-item">
+                                @if ($admin->id == auth()->user()->id)
+                                    <a>
+                                        <button class="btn btn-secondary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Delete" disabled>Make User</button>
+                                    </a> 
+                                @else
                                 <a href="/make-user/{{$admin->id}}">
                                     <button class="btn btn-primary btn-sm" type="button" data-toggle="tooltip" data-placement="top" title="Delete">Make User</button>
                                 </a>    
+                                @endif
                             </li>
                         </ul>
                     </td>
