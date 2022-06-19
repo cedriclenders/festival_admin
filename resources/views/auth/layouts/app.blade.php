2f5788be864a0d524a26bb873be064bb02b7ec05
Cedric Lenders <ceelenders@hotmail.com>
2022-06-19 13:01:13 +0200

    styling login

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset('admin/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 </head>
 
